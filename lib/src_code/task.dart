3e63f4642606f585d7f607a44fe3df8f3026a3e2
Dibas Joshi <dibasjoshi@sportsconnect.io>
2019-11-01 10:52:11 +0545

    percentage in task

--- lib/src_code/task.dart
+++ lib/src_code/task.dart
@@ -200,6 +200,7 @@ catch(e){
 
                                 Flexible(child: Text(name, textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),)),
                                 Flexible(child: Text(snapshot.data[index].parentTask.statusName, textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal))),
+                                Flexible(child: Text(snapshot.data[index].percentageComplete.toString() + "% Completed", textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal))),
 
                                 ],
 
