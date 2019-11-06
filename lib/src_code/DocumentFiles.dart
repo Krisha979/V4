4fd035d32f51ed13ac77be648596b8c94c6b8bad
ranabhawana <ranabhawana012@gmail.com>
2019-11-06 12:23:42 +0545

    document file changes

--- lib/src_code/DocumentFiles.dart
+++ lib/src_code/DocumentFiles.dart
@@ -37,7 +37,7 @@ class DocumentFilesState extends State<DocumentFilesPage> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    //Size size = MediaQuery.of(context).size;
     
   return Scaffold(
         appBar: AppBar(
@@ -97,44 +97,13 @@ class DocumentFilesState extends State<DocumentFilesPage> {
                                           color: Color(0xFFA19F9F),
                                           fontSize: 14
                                         ),),
-                                   
-                                          ]),
-                                      ),
-                                        Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        SizedBox(
-                                  height: size.height / 12,
-                                  width: size.height / 12,
-                                  
-
-                          child: url.contains(".jpeg") 
-                                          ? Image.network(details.documents[index].documentURL,
-                                             fit: BoxFit.cover,
-                                             )
-                                             :  Image(
-                        image: new AssetImage("assets/pdf.png"),
-                        height: size.height / 10,
-                      ),
-
-                                      
-                                        
-
-                                     )
-                                    
-                                        
-                                      ]
-                                      )
-                                      ]
-                                      )
-                                  ),
-                        ),
-                              ),
-                  ); 
-                }
-            )
-            )
-                          );
-  
+                                      ]),
+                                    ),
+                                  ],
+                                ),
+                              
+                            ))),
+                  );
+                }))); 
   }
 }
