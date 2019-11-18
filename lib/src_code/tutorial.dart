9648e83b65d15663222cf155c5061344ecf7767e
season maharjan <seasonmaharjan@sportsconnect.io>
2019-11-18 16:06:48 +0545

    "tutorail page "

--- lib/src_code/tutorial.dart
+++ lib/src_code/tutorial.dart
@@ -19,14 +19,7 @@ class TutorialState extends State<Tutorial>  with SingleTickerProviderStateMixin
 
   static List<Widget> imageList = [];
   static Size size;
-   // =  [
-  //   //  "assets/tutorial1.png",
-  //   //  "assets/tutorial2.png",
-  //   //  "assets/tutorial3.png",
-  //   //  "assets/tutorial4.png",
-  //   //  "assets/tutorial5.png"
-    
-  // ];
+
 int _current=0;
 
 void initState(){
@@ -46,82 +39,79 @@ static List<T> map<T>(List list, Function handler) {
 
 
 
-// final List child = map<Widget>(
-//   imageList,
-//   (index, i) {
-//     return Container(
-//       margin: EdgeInsets.all(5.0),
-//       child: ClipRRect(
-//         borderRadius: BorderRadius.all(Radius.circular(5.0)),
-//         child: Stack(children: <Widget>[
-//           Image.network(i, fit: BoxFit.cover, width: 1000.0),
-//           Positioned(
-//             bottom: 10.0,
-//             left: 0.0,
-//             right: 0.0,
-//             child: Container(
-//               decoration: BoxDecoration(
-//                 gradient: LinearGradient(
-//                   colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
-//                   begin: Alignment.bottomCenter,
-//                   end: Alignment.topCenter,
-//                 ),
-//               ),
-//               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-//               child: Text(
-//                 'No. $index image',
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20.0,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ]),
-//       ),
-//     );
-//   },
-// ).toList();
 
  Future<List<Widget>> listwidget()async {
+                  imageList.clear();
      
                   var widget1 =  Wrap(
                     children: <Widget>[
                     new Container(
-                     //height: size.height,
+                      padding: EdgeInsets.only(left: 15,right: 15,top: 15,bottom: 15),
                      width: size.width,
-             decoration:new BoxDecoration(
-                    gradient: new LinearGradient(colors:[
-                      const Color(0xFF9C38FF),
-                      const Color(0xFF8551F8),
-                    ],
-                    begin: FractionalOffset.topLeft,
-                    end: FractionalOffset.bottomRight,
-                    stops: [0.0,100.0],
-                   ),
 
-                  ),
              child: Column(
                children: <Widget>[
-                 
-                                         Image(image: AssetImage("assets/transparent.png"),
-                    height: size.height,
-                    width: size.width
+
+                                         Center(
+                                           child: Image(image: AssetImage("assets/transparent.png"),
+
                      ),
-                   
+                                         ),
+
                ]
              )
                     )
                     ]
                   );
 
-      
+                  var widget2 =  Wrap(
+                      children: <Widget>[
+                        new Container(
 
+                            width: size.width,
+                            padding: EdgeInsets.only(left: 15,right: 15,top: 15,bottom: 15),
+
+
+                            child: Column(
+                                children: <Widget>[
+
+                                  Center(
+                                    child: Image(image: AssetImage("assets/transparent.png"),
+
+                                    ),
+                                  ),
+
+                                ]
+                            )
+                        )
+                      ]
+                  );
+                  var widget3 =  Wrap(
+                      children: <Widget>[
+                        new Container(
+                          //height: size.height,
+                            width: size.width,
+                            padding: EdgeInsets.only(left: 15,right: 15,top: 15,bottom: 15),
+
+                            child: Column(
+                                children: <Widget>[
+
+                                  Center(
+                                    child: Image(image: AssetImage("assets/transparent.png"),
+
+                                    ),
+                                  ),
+
+                                ]
+                            )
+                        )
+                      ]
+                  );
 
       
             imageList.add(widget1);
-            
+            imageList.add(widget2);
+            imageList.add(widget3);
       
 
         return imageList;
@@ -140,16 +130,18 @@ return Scaffold(
        Container(
        width: size.width ,
                     height: size.height,
-                    decoration:new BoxDecoration(
-                      gradient: new LinearGradient(colors:[
-                        const Color(0xFF9C38FF),
-                        const Color(0xFF8551F8),
-                      ],
-                      begin: FractionalOffset.topLeft,
-                      end: FractionalOffset.bottomRight,
-                      stops: [0.0,100.0],
-                     ),
-                    ),
+//color: Colors.grey,
+         decoration:new BoxDecoration(
+           gradient: new LinearGradient(colors:[
+             const Color(0xFF9C38FF),
+             const Color(0xFF8551F8),
+           ],
+             begin: FractionalOffset.topLeft,
+             end: FractionalOffset.bottomRight,
+             stops: [0.0,100.0],
+           ),
+//
+         ),
 
 
                 
@@ -159,53 +151,68 @@ return Scaffold(
 
                   children: <Widget>[
 
-                     
-                    CarouselSlider(
 
-                                    items: imageList,
-                                   // child,
-                                    height:size.height,
-                                    //aspectRatio: 16/9,
-                                    viewportFraction: 0.99,
-                                    initialPage: 0,
-                                    enableInfiniteScroll: true,
-                                    reverse: false,
-                                    autoPlay: true,
-                                    enlargeCenterPage: false,
-                                    autoPlayInterval: Duration(seconds: 5),
-                                    autoPlayAnimationDuration: Duration(milliseconds: 2000),
-                                    pauseAutoPlayOnTouch: Duration(seconds: 4),
-                                    scrollDirection: Axis.horizontal,
-                                    onPageChanged: (index) {
-                                      setState(() {
-                                        _current = index;
-                                      });
-                                    },
+                     Stack(
+                      children: <Widget>[CarouselSlider(
+                        items: imageList,
+                        height:400,
+                        aspectRatio: 16/9,
+                        viewportFraction: 0.99,
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: true,
+                        enlargeCenterPage: false,
+                        autoPlayInterval: Duration(seconds: 5),
+                        autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                        pauseAutoPlayOnTouch: Duration(seconds: 4),
+                        scrollDirection: Axis.horizontal,
+                        onPageChanged: (index) {
+                          setState(() {
+                            _current = index;
+                          });
+                        },
+                      ),
+                        Positioned(
+
+                            bottom: 10.0,
+                            left: 0.0,
+                            right: 0.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: map<Widget>(imageList, (index, url) {
+                                return Container(
+                                  width: 8.0,
+                                  height: 8.0,
+                                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: _current == index ? Color.fromRGBO(21, 0, 255, 0.7) : Color.fromRGBO(0, 0, 0, 0.2)
                                   ),
-
-                                   Positioned(
-
-                                        bottom: 10.0,
-                                        left: 0.0,
-                                        right: 0.0,
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          children: map<Widget>(imageList, (index, url) {
-                                            return Container(
-                                              width: 8.0,
-                                              height: 8.0,
-                                              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                                              decoration: BoxDecoration(
-                                                  shape: BoxShape.circle,
-                                                  color: _current == index ? Color.fromRGBO(21, 0, 255, 0.7) : Color.fromRGBO(0, 0, 0, 0.2)
-                                              ),
-                                            );
-                                          }),
-                                        )
-                                    ),
+                                );
+                              }),
+                            )
+                        )
+                      ],
+                    ),
 
                     FlatButton(
-                      child: Text("Skip"),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Column(
+
+                            children: <Widget>[
+                              Text("Skip>>>",style: TextStyle( fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 20.0),),
+                            ],
+                          ),
+                        ],
+                      ),
+
 
                       onPressed: (){
                          Navigator.pushReplacement(context, CupertinoPageRoute(builder: (context) => LoginPage()));
