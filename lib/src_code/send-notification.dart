7fe4bec452ef3b16963d613c40bbc7f189e2788a
season maharjan <seasonmaharjan@sportsconnect.io>
2019-12-06 16:11:00 +0545

    "merging"

--- lib/src_code/send-notification.dart
+++ lib/src_code/send-notification.dart
@@ -267,6 +267,7 @@ Future<bool> _checkConnectivity()  async{
                                 blurRadius: 4.0,
                                 color: Colors.black.withOpacity(0.5),
                                 offset: Offset(0.0, 0.5),
+
                               ),
                             ],
                       ),   
